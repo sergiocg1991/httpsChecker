108661672355c0a7983c0a3b33a2ad49a9dd803e
Miguel <Miguel@DESKTOP-GM4DODF>
2017-07-25 19:55:36 +0200

    Agregada nueva clase

---
+++ src/main/java/DTO/ComponenteWeb.java
@@ -0,0 +1,24 @@
+package DTO;
+
+public class ComponenteWeb {
+
+	
+	private String tipo;
+	private String contenido;
+	
+	
+	
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getContenido() {
+		return contenido;
+	}
+	public void setContenido(String contenido) {
+		this.contenido = contenido;
+	}
+	
+}
